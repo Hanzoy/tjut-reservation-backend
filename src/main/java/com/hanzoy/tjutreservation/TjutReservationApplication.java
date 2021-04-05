2542460937ccd4516ae5f91f60a91287fc6d4708
hanzoy <1002745373@qq.com>
2021-04-05 21:15:00 +0800

    init

---
+++ src/main/java/com/hanzoy/tjutreservation/TjutReservationApplication.java
@@ -0,0 +1,13 @@
+package com.hanzoy.tjutreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjutReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjutReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanzoy/tjutreservation/TjutReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanzoy.tjutreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjutReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
